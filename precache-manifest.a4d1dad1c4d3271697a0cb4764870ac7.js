2fae6863d8cf6afd40acf8030643be2abf533359
BillStephens2022 <two4onebill@yahoo.com>
2023-02-03 15:20:58 -0500

    Updates

---
+++ precache-manifest.a4d1dad1c4d3271697a0cb4764870ac7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3bb4077015feb75c250f",
+    "url": "/LagunaCafe/static/css/main.e2bc07da.chunk.css"
+  },
+  {
+    "revision": "3bb4077015feb75c250f",
+    "url": "/LagunaCafe/static/js/main.12e995b9.chunk.js"
+  },
+  {
+    "revision": "40c2b4878e2db4a2a5c5",
+    "url": "/LagunaCafe/static/js/runtime~main.24cc7d63.js"
+  },
+  {
+    "revision": "8c2f33d91d6a83d32cef",
+    "url": "/LagunaCafe/static/js/2.91b64c1d.chunk.js"
+  },
+  {
+    "revision": "2d7e2623a302cc6bd1045ca8d93bc161",
+    "url": "/LagunaCafe/index.html"
+  }
+];
